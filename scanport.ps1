0b4bab3d0f5c4cf54e8fcde9d0e32b025fb7bb01
Andre Yoshimura <48725940+andreyoshimura@users.noreply.github.com>
2025-10-06 20:49:39 -0300

    Update scanport.ps1

--- scanport.ps1
+++ scanport.ps1
@@ -1,1 +1,13 @@
+$port = 5432
+$results = foreach ($h in Get-Content hosts.txt) {
+  $r = Test-NetConnection -ComputerName $h -Port $port -WarningAction SilentlyContinue
+  [PSCustomObject]@{
+    Host = $h
+    Port = $port
+    TcpTestSucceeded = $r.TcpTestSucceeded
+    RoundtripTimeMs = $r.PingReplyDetails.RoundtripTime
+  }
+}
+$results | Export-Csv -NoTypeInformation scan_5432_results.csv
+$results | Where-Object { $_.TcpTestSucceeded } | Format-Table -AutoSize
 
